b2b42d7950b1471da098f9320ddc420e534cc890
SaphireVert <jerome.cosandey@gmail.com>
2020-07-02 17:15:14 +0200

    z9wfsu0jodhgo

--- APP_hackerspace/coordonneeslol/data_gestion_hobby.py
+++ APP_hackerspace/coordonneeslol/data_gestion_hobby.py
@@ -14,7 +14,7 @@ class GestionGenres():
             # Renvoie une erreur si la connexion est perdue.
             MaBaseDeDonnee().connexion_bd.ping(False)
         except Exception as erreur:
-            flash(f"Dans Gestion coordonnees ...terrible erreur, il faut connecter une base de donnée", "danger")
+            flash(f"Dans Gestion...terrible erreur, il faut connecter une base de donnée", "danger")
             # DEBUG bon marché : Pour afficher un message dans la console.
             print(f"Exception grave Classe constructeur GestionGenres {erreur.args[0]}")
             # Ainsi on peut avoir un message d'erreur personnalisé.
@@ -27,23 +27,23 @@ class GestionGenres():
 
             # Du fait de l'utilisation des "context managers" on accède au curseur grâce au "with".
             with MaBaseDeDonnee().connexion_bd.cursor() as mc_afficher:
-                # Afficher soit la liste des coordonnees dans l'ordre inverse ou simplement le genre sélectionné
+                # Afficher soit la liste desdans l'ordre inverse ou simplement le genre sélectionné
                 # par l'action edit
                 if valeur_order_by == "ASC" and id_coordonnees_sel == 0:
-                    strsql_coordonnees_afficher = """SELECT id_coordonnees, mail FROM coordonnees ORDER BY id_coordonnees ASC"""
+                    strsql_coordonnees_afficher = """SELECT id_coordonnees, mail  ORDER BY id_coordonnees ASC"""
                     mc_afficher.execute(strsql_coordonnees_afficher)
                 elif valeur_order_by == "ASC":
                     # OM 2020.04.07 C'EST LA QUE VOUS ALLEZ DEVOIR PLACER VOTRE PROPRE LOGIQUE MySql
-                    # la commande MySql classique est "SELECT * FROM coordonnees"
+                    # la commande MySql classique est "SELECT * "
                     # Pour "lever"(raise) une erreur s'il y a des erreurs sur les noms d'attributs dans la table
                     # donc, je précise les champs à afficher
                     # Constitution d'un dictionnaire pour associer l'id du genre sélectionné avec un nom de variable
                     valeur_id_coordonnees_selected_dictionnaire = {"value_id_coordonnees_selected": id_coordonnees_sel}
-                    strsql_coordonnees_afficher = """SELECT id_coordonnees, mail FROM coordonnees  WHERE id_coordonnees = %(value_id_coordonnees_selected)s"""
+                    strsql_coordonnees_afficher = """SELECT id_coordonnees, mail   WHERE id_coordonnees = %(value_id_coordonnees_selected)s"""
                     # Envoi de la commande MySql
                     mc_afficher.execute(strsql_coordonnees_afficher, valeur_id_coordonnees_selected_dictionnaire)
                 else:
-                    strsql_coordonnees_afficher = """SELECT id_coordonnees, mail FROM coordonnees ORDER BY id_coordonnees DESC"""
+                    strsql_coordonnees_afficher = """SELECT id_coordonnees, mail  ORDER BY id_coordonnees DESC"""
                     # Envoi de la commande MySql
                     mc_afficher.execute(strsql_coordonnees_afficher)
                 # Récupère les données de la requête.
@@ -68,7 +68,7 @@ class GestionGenres():
         try:
             print(valeurs_insertion_dictionnaire)
             # OM 2020.04.07 C'EST LA QUE VOUS ALLEZ DEVOIR PLACER VOTRE PROPRE LOGIQUE MySql
-            strsql_insert_coordonnees = """INSERT INTO coordonnees (id_coordonnees,mail) VALUES (NULL,%(value_mail)s)"""
+            strsql_insert_coordonnees = """INSERT INTO(id_coordonnees,mail) VALUES (NULL,%(value_mail)s)"""
             # Du fait de l'utilisation des "context managers" on accède au curseur grâce au "with".
             # la subtilité consiste à avoir une méthode "mabd_execute" dans la classe "MaBaseDeDonnee"
             # ainsi quand elle aura terminé l'insertion des données le destructeur de la classe "MaBaseDeDonnee"
@@ -88,7 +88,7 @@ class GestionGenres():
             print(valeur_id_dictionnaire)
             # OM 2020.04.07 C'EST LA QUE VOUS ALLEZ DEVOIR PLACER VOTRE PROPRE LOGIQUE MySql
             # Commande MySql pour afficher le genre sélectionné dans le tableau dans le formulaire HTML
-            str_sql_id_coordonnees = "SELECT id_coordonnees, mail FROM coordonnees WHERE id_coordonnees = %(value_id_coordonnees)s"
+            str_sql_id_coordonnees = "SELECT id_coordonnees, mail  WHERE id_coordonnees = %(value_id_coordonnees)s"
 
             # Du fait de l'utilisation des "context managers" on accède au curseur grâce au "with".
             # la subtilité consiste à avoir une méthode "mabd_execute" dans la classe "MaBaseDeDonnee"
@@ -103,12 +103,12 @@ class GestionGenres():
 
         except Exception as erreur:
             # OM 2020.03.01 Message en cas d'échec du bon déroulement des commandes ci-dessus.
-            print(f"Problème edit_coordonnees_data Data Gestions coordonnees numéro de l'erreur : {erreur}")
-            # flash(f"Flash. Problèmes Data Gestions coordonnees numéro de l'erreur : {erreur}", "danger")
+            print(f"Problème edit_coordonnees_data Data Gestionsnuméro de l'erreur : {erreur}")
+            # flash(f"Flash. Problèmes Data Gestionsnuméro de l'erreur : {erreur}", "danger")
             # OM 2020.04.09 On dérive "Exception" par le "@obj_mon_application.errorhandler(404)" fichier "run_mon_app.py"
             # Ainsi on peut avoir un message d'erreur personnalisé.
             raise Exception(
-                "Raise exception... Problème edit_coordonnees_data d'un genre Data Gestions coordonnees {erreur}")
+                "Raise exception... Problème edit_coordonnees_data d'un genre Data Gestions{erreur}")
 
     def update_coordonnees_data (self, valeur_update_dictionnaire):
         try:
@@ -116,7 +116,7 @@ class GestionGenres():
             # OM 2019.04.02 Commande MySql pour la MODIFICATION de la valeur "CLAVIOTTEE" dans le champ "nameEditIntituleGenreHTML" du form HTML "coordonneesEdit.html"
             # le "%s" permet d'éviter des injections SQL "simples"
             # <td><input type = "text" name = "nameEditIntituleGenreHTML" value="{{ row.mail }}"/></td>
-            str_sql_update_intitulegenre = "UPDATE coordonnees SET mail = %(value_name_coordonnees)s WHERE id_coordonnees = %(value_id_coordonnees)s"
+            str_sql_update_intitulegenre = "UPDATESET mail = %(value_name_coordonnees)s WHERE id_coordonnees = %(value_id_coordonnees)s"
 
             # Du fait de l'utilisation des "context managers" on accède au curseur grâce au "with".
             # la subtilité consiste à avoir une méthode "mabd_execute" dans la classe "MaBaseDeDonnee"
@@ -133,15 +133,15 @@ class GestionGenres():
                 pymysql.IntegrityError,
                 TypeError) as erreur:
             # OM 2020.03.01 Message en cas d'échec du bon déroulement des commandes ci-dessus.
-            print(f"Problème update_coordonnees_data Data Gestions coordonnees numéro de l'erreur : {erreur}")
-            # flash(f"Flash. Problèmes Data Gestions coordonnees numéro de l'erreur : {erreur}", "danger")
-            # raise Exception('Raise exception... Problème update_coordonnees_data d\'un genre Data Gestions coordonnees {}'.format(str(erreur)))
+            print(f"Problème update_coordonnees_data Data Gestionsnuméro de l'erreur : {erreur}")
+            # flash(f"Flash. Problèmes Data Gestionsnuméro de l'erreur : {erreur}", "danger")
+            # raise Exception('Raise exception... Problème update_coordonnees_data d\'un genre Data Gestions{}'.format(str(erreur)))
             if erreur.args[0] == 1062:
                 flash(f"Flash. Cette valeur existe déjà : {erreur}", "danger")
                 # Deux façons de communiquer une erreur causée par l'insertion d'une valeur à double.
                 flash(f"'Doublon !!! Introduire une valeur différente", "warning")
                 # Message en cas d'échec du bon déroulement des commandes ci-dessus.
-                print(f"Problème update_coordonnees_data Data Gestions coordonnees numéro de l'erreur : {erreur}")
+                print(f"Problème update_coordonnees_data Data Gestionsnuméro de l'erreur : {erreur}")
 
                 raise Exception("Raise exception... Problème update_coordonnees_data d'un genre DataGestionscoordonnees {erreur}")
 
@@ -154,7 +154,7 @@ class GestionGenres():
 
             # OM 2020.04.07 C'EST LA QUE VOUS ALLEZ DEVOIR PLACER VOTRE PROPRE LOGIQUE MySql
             # Commande MySql pour afficher le genre sélectionné dans le tableau dans le formulaire HTML
-            str_sql_select_id_coordonnees = "SELECT id_coordonnees, mail FROM coordonnees WHERE id_coordonnees = %(value_id_coordonnees)s"
+            str_sql_select_id_coordonnees = "SELECT id_coordonnees, mail  WHERE id_coordonnees = %(value_id_coordonnees)s"
 
             # Du fait de l'utilisation des "context managers" on accède au curseur grâce au "with".
             # la subtilité consiste à avoir une méthode "mabd_execute" dans la classe "MaBaseDeDonnee"
@@ -168,9 +168,9 @@ class GestionGenres():
 
 
             # Affiche tous les films qui ont le genre que l'utilisateur veut effacer
-            str_sql_coordonnees_films_delete = """SELECT id_coordonnees_film, nom_film, id_coordonnees, mail FROM coordonnees_films
+            str_sql_coordonnees_films_delete = """SELECT id_coordonnees_film, nom_film, id_coordonnees, mail _films
                                             INNER JOIN t_films ON coordonnees_films.fk_film = t_films.id_film
-                                            INNER JOIN coordonnees ON coordonnees_films.fk_coordonnees = coordonnees.id_coordonnees
+                                            INNER JOINON coordonnees_films.fk_coordonnees = coordonnees.id_coordonnees
                                             WHERE fk_coordonnees = %(value_id_coordonnees)s"""
             with MaBaseDeDonnee().connexion_bd as mconn_bd:
                 with mconn_bd as mc_cur:
@@ -187,11 +187,11 @@ class GestionGenres():
                 pymysql.IntegrityError,
                 TypeError) as erreur:
             # DEBUG bon marché : Pour afficher un message dans la console.
-            print(f"Problème delete_select_coordonnees_data Gestions coordonnees numéro de l'erreur : {erreur}")
+            print(f"Problème delete_select_coordonnees_data Gestionsnuméro de l'erreur : {erreur}")
             # C'est une erreur à signaler à l'utilisateur de cette application WEB.
             flash(f"Flash. Problème delete_select_coordonnees_data numéro de l'erreur : {erreur}", "danger")
             raise Exception(
-                "Raise exception... Problème delete_select_coordonnees_data d\'un genre Data Gestions coordonnees {erreur}")
+                "Raise exception... Problème delete_select_coordonnees_data d\'un genre Data Gestions{erreur}")
 
     def delete_coordonnees_data (self, valeur_delete_dictionnaire):
         try:
@@ -199,9 +199,9 @@ class GestionGenres():
             # OM 2019.04.02 Commande MySql pour EFFACER la valeur sélectionnée par le "bouton" du form HTML "coordonneesEdit.html"
             # le "%s" permet d'éviter des injections SQL "simples"
             # <td><input type = "text" name = "nameEditIntituleGenreHTML" value="{{ row.mail }}"/></td>
-            str_sql_delete_films_coordonnees = """DELETE FROM coordonnees_films WHERE fk_coordonnees = %(value_id_coordonnees)s"""
+            str_sql_delete_films_coordonnees = """DELETE _films WHERE fk_coordonnees = %(value_id_coordonnees)s"""
 
-            str_sql_delete_intitulegenre = "DELETE FROM coordonnees WHERE id_coordonnees = %(value_id_coordonnees)s"
+            str_sql_delete_intitulegenre = "DELETE  WHERE id_coordonnees = %(value_id_coordonnees)s"
 
             # Du fait de l'utilisation des "context managers" on accède au curseur grâce au "with".
             # la subtilité consiste à avoir une méthode "mabd_execute" dans la classe "MaBaseDeDonnee"
@@ -225,8 +225,8 @@ class GestionGenres():
                 pymysql.IntegrityError,
                 TypeError) as erreur:
             # DEBUG bon marché : Pour afficher un message dans la console.
-            print(f"Problème delete_coordonnees_data Data Gestions coordonnees numéro de l'erreur : {erreur}")
-            # flash(f"Flash. Problèmes Data Gestions coordonnees numéro de l'erreur : {erreur}", "danger")
+            print(f"Problème delete_coordonnees_data Data Gestionsnuméro de l'erreur : {erreur}")
+            # flash(f"Flash. Problèmes Data Gestionsnuméro de l'erreur : {erreur}", "danger")
             if erreur.args[0] == 1451:
                 # OM 2020.04.09 Traitement spécifique de l'erreur 1451 Cannot delete or update a parent row: a foreign key constraint fails
                 # en MySql le moteur INNODB empêche d'effacer un genre qui est associé à un film dans la table intermédiaire "coordonnees_films"
